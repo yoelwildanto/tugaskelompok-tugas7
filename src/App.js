73f2ad48915be94a018bbf04f1a4bef04ad3acff
Murp13 <suryarifin55@gmail.com>
2023-10-02 15:18:49 +0700

    Add New File

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from 'react';
+import Stopwatch from './nomer2/stopwatch';
 
 function App() {
   return (
     <div className="App">
-      
+      <Stopwatch />
     </div>
   );
 }
