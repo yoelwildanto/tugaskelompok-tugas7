b1c6cbcc4e6babe179797ac014674e335e281564
Murp13 <suryarifin55@gmail.com>
2023-10-03 06:37:40 +0700

    Add Fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { ChakraProvider} from '@chakra-ui/react';
 import './App.css'
 // import AppRouter from './Router';
-import { Route,Routes } from 'react-router-dom';
-import HomePage from './nomer5/HomePage';
-import ListPage from './nomer5/ListPage';
+// import { Route,Routes } from 'react-router-dom';
+// import HomePage from './nomer5/HomePage';
+// import ListPage from './nomer5/ListPage';
+import Stopwatch from './nomer2/stopwatch';
 
 
 
@@ -12,11 +13,12 @@ function App() {
   return (
     <ChakraProvider>
       <div className="App">
-      <Routes>
+      {/* <Routes>
         <Route path='/' element={
         <HomePage/>}/>
         <Route path="/list" element={<ListPage/>}/>
-      </Routes>
+      </Routes> */}
+          <Stopwatch></Stopwatch>
         
       </div>
     </ChakraProvider>
